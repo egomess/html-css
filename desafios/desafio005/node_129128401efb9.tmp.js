7d4c12b1c9e2bffec30bd3f0eab3bf7c182778a1
imgomess <162859855+egomess@users.noreply.github.com>
2025-02-27 15:23:46 -0300

    Initial commit

---
+++ desafios/desafio005/node_129128401efb9.tmp.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social</title>
+    <style>
+        foto de perfil{
+            size: ;
+        }
+    </style>
+</head>
+<body>
+    <h1>Minhas redes sociais</h1>
+    <h2>Quem sou eu?</h2>
+    <img src="imagens/foto-perfil-150.png" alt="foto de perfil">
+    <p>Meu nome é <strong>Enzo Luz Gomes</strong>, tenho 15 anos e sou um estudante de HTML e CSS <br> em busca de uma vaga no mercado de trabalho como um programador full-stack. <br> Tenho muito foco e determinação e acredito que tudo dará certo! </p>
+
+    <h2>Como falar comigo?</h2>
+    <ul>
+        <li><img src="imagens/icone-youtube.png" alt="icone youtube"><a href="https://www.youtube.com/@GomesSSsz" target="_blank" rel="external"> /Gomes</a> - Acesse o meu canal do YouTube</li>
+        <li><img src="imagens/icone-github.png" alt="logo github"><a href="https://github.com/imgomess">/imgomess</a> - Acesse o meu perfil de estudos do GitHub</li>
+        <li><img src="imagens/icone-instagram.png" alt="logo instagram"><a href="https://www.instagram.com/im.gomesss/">/im.gomesss</a> - Me siga no Instagram</li>
+        <li><img src="imagens/icone-linkedin.png" alt="logo linkedin"><a href="https://www.linkedin.com/in/enzo-gomes-6020152bb/">/enzo-gomes</a> - Me adiciona lá no LinkedIn</li>
+        <Li><img src="imagens/icone-twitter.png" alt="logo twitter"><a href="https://twitter.com/NeOZzZzZ19">/Gomess&#x1F920</a> - Me segue lá no Twitter</Li>
+    </ul>
+</body>
+</html>
